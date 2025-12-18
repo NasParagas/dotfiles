69e476d9b43fa6ce815f0351213be37d9802ee46
NasParagas <naspara4693@gmail.com>
2025-12-19 08:32:25 +0900

    [delete]

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,6 +12,3 @@ require("config.option")
 require("config.keymap")
 require("config.autocommand")
 require("config.lazy")
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
