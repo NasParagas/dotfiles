5b25c0bed47a30286a94a45b947df0b5427b2129
NasParagas <naspara4693@gmail.com>
2025-12-19 08:28:18 +0900

    add: C++のタブ幅とかの設定

--- .config/nvim/lua/config/autocommand.lua
+++ .config/nvim/lua/config/autocommand.lua
@@ -12,17 +12,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- C/C++ のときはタブ文字を使い、見た目の幅は 4
+-- C/C++
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "c", "cpp" },
 	callback = function()
 		vim.bo.expandtab = false -- タブ文字を挿入
 		vim.bo.tabstop = 4 -- タブ表示幅
 		vim.bo.shiftwidth = 4 -- >>, << のステップ
-		vim.bo.softtabstop = 0 -- <BS> でタブを壊さないなら 0 推奨（好みで 4 でも可）
 		vim.bo.autoindent = true -- 改行時に直前行のインデントをコピー
 		vim.bo.cindent = true -- C/C++ の構文に沿ったインデント支援
-		-- 必要なら cindent の挙動調整（例）
-		-- vim.bo.cinoptions = "l1,g0,t0"
 	end,
 })
