dd5e45b1b926139253d725bde0732bc086af1c2f
NasParagas <naspara4693@gmail.com>
2026-02-11 22:29:47 +0900

    add: image preview plugin.

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+-- lazy.nvim
+return
+-- lazy.nvim
+{
+	"folke/snacks.nvim",
+	opts = {
+		image = {
+			-- your image configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			doc = {
+				enabled = true,
+				float = true,
+			},
+		},
+	},
+}
