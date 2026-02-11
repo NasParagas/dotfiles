3fa2c7ddde5b2a8686ca047c74e8de8206f60bcb
NasParagas <naspara4693@gmail.com>
2026-02-11 13:27:16 +0900

    update: 起動時にload

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -1,13 +1,11 @@
 return {
 	{
 		"romgrk/barbar.nvim",
+		event = "VimEnter",
 		dependencies = {
 			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
 			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
 		},
-		init = function()
-			vim.g.barbar_auto_setup = false
-		end,
 		opts = {
 			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
 			-- animation = true,
