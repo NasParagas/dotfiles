2f1c417addfc48e73f50cc6a91ae3923ee415ebb
NasParagas <naspara4693@gmail.com>
2026-02-21 11:53:21 +0900

    Revert "refactor:"
    This reverts commit 115d3c556942c0e07d25b21c297b68921d0f8e87.

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -10,10 +10,12 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
-vim.keymap.set("n", "<C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", { desc = "Move focus to the upper window" })
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- jkでinsert -> normal
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
