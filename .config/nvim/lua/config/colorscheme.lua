70a8d73696abfdb64b9ef798b5c568cad8d9ce65
NasParagas <naspara4693@gmail.com>
2026-02-22 17:58:25 +0900

    add: colorscheme setting

---
+++ .config/nvim/lua/config/colorscheme.lua
@@ -0,0 +1,16 @@
+local function set_colorscheme(theme_name)
+	local status_ok, err = pcall(vim.cmd, "colorscheme " .. theme_name)
+	if not status_ok then
+		vim.notify("error: " .. err, vim.log.levels.WARN)
+		vim.cmd("colorscheme default")
+	end
+end
+
+-- background setting
+-- vim.o.background = "light"
+vim.o.background = "dark"
+
+-- colorscheme setting
+-- set_theme("catppuccin")
+set_colorscheme("gruvbox")
+-- set_theme("tokyonight")
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,59 +1,40 @@
 return {
-	-- {
-	-- 	"catppuccin/nvim",
-	-- 	name = "catppuccin",
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("catppuccin").setup({
-	-- 			transparent_background = true,
-	-- 			styles = {
-	-- 				comments = {},
-	-- 				conditionals = { "italic" },
-	-- 				loops = {},
-	-- 				functions = {},
-	-- 				keywords = {},
-	-- 				strings = {},
-	-- 				variables = {},
-	-- 				numbers = {},
-	-- 				booleans = {},
-	-- 				properties = {},
-	-- 				types = {},
-	-- 				operators = {},
-	-- 			},
-	-- 		})
-	-- 		vim.cmd.colorscheme("catppuccin")
-	-- 	end,
-	-- },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			transparent_background = true,
+			styles = {},
+		},
+		-- config = function(opts)
+		-- 	require("catppuccin").setup(opts)
+		-- 		vim.cmd("colorscheme catppuccin")
+		-- end,
+	},
 	{
 		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
 		opts = {
-			terminal_colors = true,
-			undercurl = true,
-			underline = true,
-			bold = true,
-			italic = {
-				strings = true,
-				emphasis = true,
-				comments = true,
-				operators = false,
-				folds = true,
-			},
-			strikethrough = true,
-			invert_selection = false,
-			invert_signs = false,
-			invert_tabline = false,
-			inverse = true,
-			contrast = "",
-			palette_overrides = {},
-			overrides = {},
-			dim_inactive = false,
+			-- style depends on backgroud theme
 			transparent_mode = true,
 		},
-		-- config関数で opts を受け取って適用する
-		config = function(_, opts)
-			require("gruvbox").setup(opts)
-			vim.cmd("colorscheme gruvbox")
-		end,
+		-- config = function(_, opts)
+		-- 	require("gruvbox").setup(opts)
+		-- 	vim.cmd("colorscheme gruvbox")
+		-- end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		opts = {
+			style = "night", -- moon, night, day, storm
+			transparent = true,
+		},
+		-- config = function(opts)
+		-- 	require("tokyonight").setup(opts)
+		-- 	vim.cmd("colorscheme tokyonight")
+		-- end,
 	},
 }
