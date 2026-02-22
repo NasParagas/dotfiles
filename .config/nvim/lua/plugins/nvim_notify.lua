744cd580a8dfd23ec2b8c9a9017f1aa2a5f2e822
NasParagas <naspara4693@gmail.com>
2026-02-22 12:04:14 +0900

    add:

---
+++ .config/nvim/lua/plugins/nvim_notify.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"rcarriga/nvim-notify",
+		event = "VeryLazy",
+
+		opts = {
+			top_down = false,
+			render = "minimal", -- default, minimal, simple, compact
+			timeout = 7000, -- Time until notification disappear
+		},
+
+		config = function(_, opts)
+			require("notify").setup(opts)
+			-- replace
+			vim.notify = require("notify")
+		end,
+	},
+}
