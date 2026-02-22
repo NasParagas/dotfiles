006ff2c03ad729055f0cde0d567b91e9a7de51fa
NasParagas <naspara4693@gmail.com>
2026-02-22 23:14:49 +0900

    refactor:

--- .config/nvim/lua/plugins/render_markdown.lua
+++ .config/nvim/lua/plugins/render_markdown.lua
@@ -4,45 +4,7 @@ return {
 	event = "BufEnter",
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = {
-		heading = {
-			enabled = true,
-			render_modes = false,
-			atx = true,
-			setext = true,
-			sign = true,
-			icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
-			position = "overlay",
-			signs = { "󰫎 " },
-			width = "full",
-			left_margin = 0,
-			left_pad = 0,
-			right_pad = 0,
-			min_width = 0,
-			border = false,
-			border_virtual = false,
-			border_prefix = false,
-			above = "▄",
-			below = "▀",
-			backgrounds = {
-				"RenderMarkdownH1Bg",
-				"RenderMarkdownH2Bg",
-				"RenderMarkdownH3Bg",
-				"RenderMarkdownH4Bg",
-				"RenderMarkdownH5Bg",
-				"RenderMarkdownH6Bg",
-			},
-			foregrounds = {
-				"RenderMarkdownH1",
-				"RenderMarkdownH2",
-				"RenderMarkdownH3",
-				"RenderMarkdownH4",
-				"RenderMarkdownH5",
-				"RenderMarkdownH6",
-			},
-			custom = {},
-		},
-	},
+	opts = {},
 	config = function(opts)
 		require("render-markdown").setup(opts)
 	end,
