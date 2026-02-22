3e179ab9ab80ee99915dba35798c4b169399eaa7
NasParagas <naspara4693@gmail.com>
2026-02-22 23:17:49 +0900

    refactor:

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,33 +3,17 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
-		opts = {
-			flavour = "macchiato", -- latte(light), frappe, macchiato, mocha
-			transparent_background = true,
-			styles = {},
-		},
 	},
 	{
 		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
-		opts = {
-			-- style depends on backgroud theme
-			transparent_mode = true,
-		},
 	},
 	{
 		"folke/tokyonight.nvim",
 		priority = 1000,
-		opts = {
-			style = "night", -- moon, night, day, storm
-			transparent = true,
-		},
 	},
 	{
 		"rebelot/kanagawa.nvim",
 		priority = 1000,
-		opts = {
-			transparent = true,
-		},
 	},
 }
