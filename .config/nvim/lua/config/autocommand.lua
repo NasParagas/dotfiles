11e152a6dbfc7140e9337906e03eb907464fd7b5
NasParagas <naspara4693@gmail.com>
2026-02-25 09:05:15 +0900

    update: indent, tab, space setting

--- .config/nvim/lua/config/autocommand.lua
+++ .config/nvim/lua/config/autocommand.lua
@@ -12,15 +12,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- file setting
+-- all
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- C/C++
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "c", "cpp" },
 	callback = function()
-		vim.bo.expandtab = false -- タブ文字を挿入
-		vim.bo.tabstop = 4 -- タブ表示幅
-		vim.bo.shiftwidth = 4 -- >>, << のステップ
-		vim.bo.autoindent = true -- 改行時に直前行のインデントをコピー
-		vim.bo.cindent = true -- C/C++ の構文に沿ったインデント支援
+		vim.opt_local.autoindent = true
+		vim.opt_local.cindent = true -- C/C++ の構文に沿ったインデント支援
 	end,
 })
 
@@ -35,16 +38,10 @@ vim.api.nvim_create_autocmd("FileType", {
 		-- b=blank required
 		-- ` -`で始まる行について行うという設定...のはず
 		vim.opt_local.comments = "b:-,b:*,b:+,n:>"
-		-- タブ文字の幅をスペース2つ分に見せる
 		vim.opt_local.tabstop = 2
-
-		-- インデント（>> や <<）の幅をスペース2つ分にする
 		vim.opt_local.shiftwidth = 2
-
-		-- Tabキーを押した時に、タブ文字ではなくスペースを挿入する
-		vim.opt_local.expandtab = true
-
 		-- バックスペースキーでスペース2つ分を一度に消せるようにする（あたかもタブのように振る舞う）
 		vim.opt_local.softtabstop = 2
+		vim.keymap.set("i", "<S-Enter>", "<CR><Esc>S", { buffer = true, remap = false })
 	end,
 })
