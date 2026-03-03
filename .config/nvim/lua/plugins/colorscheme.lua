059b756459c49b346080007efdfc5d31a789cdbc
NasParagas <naspara4693@gmail.com>
2026-03-04 08:11:39 +0900

    refactor:

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,18 +2,27 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
+		lazy = false,
 		priority = 1000,
 	},
 	{
 		"ellisonleao/gruvbox.nvim",
+		lazy = false,
 		priority = 1000,
 	},
 	{
 		"folke/tokyonight.nvim",
+		lazy = false,
 		priority = 1000,
 	},
 	{
 		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"neanias/everforest-nvim",
+		lazy = false,
 		priority = 1000,
 	},
 }
