10a4b355e80651e37130cd8e283f233a6a224722
NasParagas <naspara4693@gmail.com>
2026-03-30 08:24:53 +0900

    update: default off

--- .config/nvim/lua/plugins/random_key_tips.lua
+++ .config/nvim/lua/plugins/random_key_tips.lua
@@ -2,6 +2,10 @@ return {
 	"NasParagas/random-key-tips.nvim",
 	event = "VeryLazy",
 	config = function()
-		require("random-key-tips").setup({ interval = 15000 })
+		-- Start automatically when Neovim loads
+		require("random-key-tips").setup({
+			interval = 15000,
+			-- auto_start = true,
+		})
 	end,
 }
