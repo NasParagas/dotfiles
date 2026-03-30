521eb7a693ef9766fac2e6b1c439a566061f8029
NasParagas <naspara4693@gmail.com>
2026-03-30 09:33:17 +0900

    add: insert details tag command

--- .config/nvim/lua/config/autocommand.lua
+++ .config/nvim/lua/config/autocommand.lua
@@ -7,6 +7,26 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- Create a user command named 'InsertDetails'
+vim.api.nvim_create_user_command("InsertDetails", function()
+	-- Define the standard HTML details/summary lines
+	local lines = {
+		"<details>",
+		"  <summary> </summary>",
+		"  ",
+		"</details>",
+	}
+
+	-- Get the current cursor position (row is 1-indexed)
+	local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+
+	-- Insert the lines immediately below the current cursor
+	vim.api.nvim_buf_set_lines(0, row, row, false, lines)
+
+	-- Move the cursor into the <summary> tag for quick editing
+	vim.api.nvim_win_set_cursor(0, { row + 2, 11 })
+end, { desc = "Insert HTML details and summary tags" })
+
 ----- file setting -----
 -- all
 vim.opt.expandtab = true
